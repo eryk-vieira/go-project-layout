ab5da41626fd96d94219bad8246360904c8853c3
Eryk-Luiz <erykluizvieira@gmail.com>
2023-07-21 09:22:10 -0300

    Inject server dependencias on main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"github.com/eryk-vieira/go-api-project-layout/internal/adapters/api/rest"
+	"github.com/eryk-vieira/go-api-project-layout/internal/adapters/api/rest/handlers"
+	"github.com/eryk-vieira/go-api-project-layout/internal/adapters/database/postgres"
+	"github.com/eryk-vieira/go-api-project-layout/internal/adapters/database/postgres/repository"
+	"github.com/eryk-vieira/go-api-project-layout/internal/core/service"
 )
 
 func main() {
-	rest.RunServer()
+	appHandler := handlers.NewAppHandler()
+	userHandler := injectUserHandlerDependencies()
+
+	rest.RunServer(appHandler, userHandler)
+}
+
+func injectUserHandlerDependencies() *handlers.UserHandler {
+	userRepository := repository.NewUserRepository(postgres.GetDatabaseInstance())
+	userService := service.NewUserService(userRepository)
+	userHandler := handlers.NewUserHandler(userService)
+
+	return userHandler
 }
--- internal/adapters/api/rest/rest.go
+++ internal/adapters/api/rest/rest.go
@@ -2,31 +2,19 @@ package rest
 
 import (
 	"github.com/eryk-vieira/go-api-project-layout/internal/adapters/api/rest/handlers"
-	"github.com/eryk-vieira/go-api-project-layout/internal/adapters/database/postgres"
-	"github.com/eryk-vieira/go-api-project-layout/internal/adapters/database/postgres/repository"
-	"github.com/eryk-vieira/go-api-project-layout/internal/core/port"
-	"github.com/eryk-vieira/go-api-project-layout/internal/core/service"
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	userService port.UserService
-)
-
-func init() {
-	userRepository := repository.NewUserRepository(postgres.GetDatabaseInstance())
-	userService = service.NewUserService(userRepository)
-}
-
-func RunServer() {
+func RunServer(
+	appHandler *handlers.AppHandler,
+	userHandler *handlers.UserHandler,
+) {
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
 
-	appHandler := handlers.NewAppHandler()
 	v1.GET("/health-check", appHandler.HealthCheck)
 
-	userHandler := handlers.NewUserHandler(userService)
 	users := v1.Group("/users")
 	users.POST("/users", userHandler.CreateUser)
 
